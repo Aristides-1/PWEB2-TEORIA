7696883473e2855b5ac9600238c0082adeb743e0
daigo <dcervantesapa@unsa.edu.pe>
2025-06-23 00:08:36 -0500

    corriginedo funcion textfield para que sea exitosa la migracion

--- avancesDjango/listaContactos/personas/models.py
+++ avancesDjango/listaContactos/personas/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 #Atributos de clase persona
 class Personas(models.Model):
-    nombres=models.textField()
-    apellidos=models.textField()
+    nombres=models.TextField()
+    apellidos=models.TextField()
