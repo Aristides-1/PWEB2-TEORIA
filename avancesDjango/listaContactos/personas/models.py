0788356d951da12b86cbc2749e860062870267f4
daigo <dcervantesapa@unsa.edu.pe>
2025-06-24 01:44:31 -0500

    corrigiendo error sintaxis

--- avancesDjango/listaContactos/personas/models.py
+++ avancesDjango/listaContactos/personas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 #Atributos de clase persona
 class Personas(models.Model):
     nombres=models.CharField(max_length=30)
-    apellidos=models.Charfield(max_length=40)
+    apellidos=models.CharField(max_length=40)
     edad=models.IntegerField()
 
     def __str__(self):
