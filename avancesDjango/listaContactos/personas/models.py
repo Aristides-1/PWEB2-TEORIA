7214bae0cca0c1b2895b1c01e344949548c93ed7
daigo <dcervantesapa@unsa.edu.pe>
2025-06-23 23:12:33 -0500

    cambiando aplicacion personas con fieltypes y a posterior tipos de camnpos

--- avancesDjango/listaContactos/personas/models.py
+++ avancesDjango/listaContactos/personas/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Personas(models.Model):
     nombres=models.CharField(max_length=30)
     apellidos=models.Charfield(max_length=40)
-    edad=models.IntegerField().(max_digits=3)
+    edad=models.IntegerField()
 
     def __str__(self):
         return self.nombres
