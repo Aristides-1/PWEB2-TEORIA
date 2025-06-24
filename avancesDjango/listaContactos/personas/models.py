fdf9143e9513939ec31afe746dbe4627e3a443b4
daigo <dcervantesapa@unsa.edu.pe>
2025-06-23 22:57:52 -0500

    nuevos modelos integrados a la aplicacion personas con fieldtypes, mejor restriccion de datos ingresados

--- avancesDjango/listaContactos/personas/models.py
+++ avancesDjango/listaContactos/personas/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 #Atributos de clase persona
 class Personas(models.Model):
-    nombres=models.TextField()
-    apellidos=models.TextField()
+    nombres=models.CharField(max_length=30)
+    apellidos=models.Charfield(max_length=40)
+    edad=models.IntegerField().(max_digits=3)
 
     def __str__(self):
         return self.nombres
