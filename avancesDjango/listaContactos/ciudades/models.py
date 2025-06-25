0580760d6c23063ee7eba57adc76d5dc00ff2f40
daigo <dcervantesapa@unsa.edu.pe>
2025-06-24 23:36:56 -0500

    cambiando tipo de dato aplicacion ciudades

--- avancesDjango/listaContactos/ciudades/models.py
+++ avancesDjango/listaContactos/ciudades/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 #Atributos de clase ciudades
 class Ciudades(models.Model):
-    ciudad=models.TextField()
+    ciudad=models.CharField(max.length=30)
 
     def __str__(self):
         return self.ciudad
