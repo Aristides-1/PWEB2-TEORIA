fe42ee3b6bfccdf420b48c3ec94e928330f586c8
daigo <dcervantesapa@unsa.edu.pe>
2025-06-24 23:39:12 -0500

    corrigiendo models aplicacion ciudades

--- avancesDjango/listaContactos/ciudades/models.py
+++ avancesDjango/listaContactos/ciudades/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 #Atributos de clase ciudades
 class Ciudades(models.Model):
-    ciudad=models.CharField(max.length=30)
+    ciudad=models.CharField(max_length=30)
 
     def __str__(self):
         return self.ciudad
