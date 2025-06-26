700017a3563e0c033b209a5dc4e303909305800e
daigo <dcervantesapa@unsa.edu.pe>
2025-06-26 15:32:14 -0500

    agregando models a aplicaciones distintas

--- avancesDjango/listaContactos/ciudades/models.py
+++ avancesDjango/listaContactos/ciudades/models.py
@@ -4,7 +4,6 @@ from django.db import models
 #Atributos de clase ciudades
 class Ciudades(models.Model):
     ciudad=models.CharField(max_length=30)
-    pais=models.CharField(max_length=15)
 
     def __str__(self):
         return self.ciudad
--- avancesDjango/listaContactos/personas/models.py
+++ avancesDjango/listaContactos/personas/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 #Atributos de clase persona
 class Personas(models.Model):
-    nombres=models.CharField(max_length=30)
-    apellidos=models.CharField(max_length=40)
-    edad=models.IntegerField()
+    nombres=models.CharField(max_length=25)
+    apellidos=models.CharField(max_length=35)
+    edad = models.PositiveSmallIntegerField()
+
 
     def __str__(self):
         return self.nombres
