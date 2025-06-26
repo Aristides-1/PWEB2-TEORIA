5758aa7ee3f776443c11f1b37266d7a52409f2cc
daigo <dcervantesapa@unsa.edu.pe>
2025-06-26 16:41:19 -0500

    agregando un nuevo campo en plena ejecución de server django

--- avancesDjango/listaContactos/personas/models.py
+++ avancesDjango/listaContactos/personas/models.py
@@ -5,7 +5,7 @@ class Personas(models.Model):
     nombres=models.CharField(max_length=25)
     apellidos=models.CharField(max_length=35)
     edad = models.PositiveSmallIntegerField()
-
+    donador=models.BooleanField()
 
     def __str__(self):
         return self.nombres
