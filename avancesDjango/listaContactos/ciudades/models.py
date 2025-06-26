f10d71be5e8153bb2298185d1fbb6705d8d17fb2
daigo <dcervantesapa@unsa.edu.pe>
2025-06-26 15:24:06 -0500

    agregando nuevas clases al proyecto django2

--- avancesDjango/listaContactos/ciudades/models.py
+++ avancesDjango/listaContactos/ciudades/models.py
@@ -4,6 +4,7 @@ from django.db import models
 #Atributos de clase ciudades
 class Ciudades(models.Model):
     ciudad=models.CharField(max_length=30)
+    pais=models.CharField(max_length=15)
 
     def __str__(self):
         return self.ciudad
