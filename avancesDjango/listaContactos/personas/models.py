d53c73bdc544a406ee28cddfe77a995dc1bfaeb7
daigo <dcervantesapa@unsa.edu.pe>
2025-06-26 22:10:53 -0500

    agregando validez a campos vacios o no vacios

--- avancesDjango/listaContactos/personas/models.py
+++ avancesDjango/listaContactos/personas/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 #Atributos de clase persona
 class Personas(models.Model):
-    nombres=models.CharField(max_length=25)
+    nombres=models.CharField(max_length=25, blank=False, null=False)
     apellidos=models.CharField(max_length=35)
     edad = models.PositiveSmallIntegerField()
-    donador=models.BooleanField()
+    donador=models.BooleanField(defaul=False)
 
     def __str__(self):
         return self.nombres
