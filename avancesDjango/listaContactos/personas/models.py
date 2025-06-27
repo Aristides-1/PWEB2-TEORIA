2d7b58b91428785d1d941f2f1660aa89117e4fbe
daigo <dcervantesapa@unsa.edu.pe>
2025-06-26 22:12:55 -0500

    correcion codigo

--- avancesDjango/listaContactos/personas/models.py
+++ avancesDjango/listaContactos/personas/models.py
@@ -5,7 +5,7 @@ class Personas(models.Model):
     nombres=models.CharField(max_length=25, blank=False, null=False)
     apellidos=models.CharField(max_length=35)
     edad = models.PositiveSmallIntegerField()
-    donador=models.BooleanField(defaul=False)
+    donador=models.BooleanField(default=False)
 
     def __str__(self):
         return self.nombres
