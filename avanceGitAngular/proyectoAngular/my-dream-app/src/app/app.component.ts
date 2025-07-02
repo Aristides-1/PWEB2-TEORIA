560be7487081ae19af4e3d104072bf38827990a9
Aristides-1 <dcervantesapa@unsa.edu.pe>
2025-07-02 15:51:16 -0500

    revision app.component.s

--- avanceGitAngular/proyectoAngular/my-dream-app/src/app/app.component.ts
+++ avanceGitAngular/proyectoAngular/my-dream-app/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; // <-- Aquí agregamos la importación de RouterModule
 
 @Component({
   selector: 'app-root',
+  standalone: true,               // Esto indica que es standalone
+  imports: [RouterModule],        // Aquí declaramos que usamos RouterModule
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-dream-app';
-  name = 'Carlo Corrales';
-  email = 'ccorrales@unsa.edu.pe';
+  name = 'Diego Cervantes';
+  email = 'dcervantesapa@unsa.edu.pe';
   webpage = 'http://www.unsa.edu.pe';
 }
