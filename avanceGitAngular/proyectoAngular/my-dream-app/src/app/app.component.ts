8407f7daf8b02746ab4a4efdf3a1f1729f07a033
Aristides-1 <dcervantesapa@unsa.edu.pe>
2025-07-02 10:20:42 -0500

    editando app.component.ts

---
+++ avanceGitAngular/proyectoAngular/my-dream-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-dream-app';
+  name = 'Carlo Corrales';
+  email = 'ccorrales@unsa.edu.pe';
+  webpage = 'http://www.unsa.edu.pe';
+}
