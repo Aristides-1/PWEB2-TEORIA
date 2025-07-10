3be12ac910b40b4ead17dcb5ab468aaf27866f27
Aristides-1 <dcervantesapa@unsa.edu.pe>
2025-07-10 11:57:53 -0500

    creando modelo movie para añadir peliculas y sus propiedades

--- djangular/api/models.py
+++ djangular/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+#Creamos modelo "Movie" que tiene atributos de título, 
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    desc = models.CharField(max_length=256)
+    year = models.IntegerField()
