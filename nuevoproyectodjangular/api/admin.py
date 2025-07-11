652502f987d3ab569374a6e105bc34ea5650531f
Aristides-1 <dcervantesapa@unsa.edu.pe>
2025-07-10 22:25:24 -0500

    errores en aplicacion corregido 2

--- nuevoproyectodjangular/api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Movie
-
-#Modelo movie en el registro
-admin.site.register(Movie)
--- nuevoproyectodjangular/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User,Group
-from rest_framework import viewsets
-from .serializer import MovieSerializer
-from .models import Movie
-
-class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
